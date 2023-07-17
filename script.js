694bcbc5aabd2ad6c6d621c3a315c69c73eb8d3b
Jacqueline Lee <jacquelinelee@Jacquelines-MBP.home>
2023-07-17 10:48:05 -0700

    add script to html

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('emailForm').addEventListener('submit', (event) => {
+     event.preventDefault();
+
+     var email = document.getElementById('email').value;
+
+     console.log('Email', email);
+})
